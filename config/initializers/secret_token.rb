2b99d1d3e8209a58df737ef8bbfdf604680e8301
darchcruise <darchcruise@yahoo.com>
2013-08-02 17:08:45 -0400

    begin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChooseYourOwnAdventure::Application.config.secret_token = 'c0559adff30dd7a29785d18794242bd7982e02163d7c52602f30672afc00eeb5cc9feec19c331f78774fd618ff87b567c0deede925f818a052890573434e8e49'
