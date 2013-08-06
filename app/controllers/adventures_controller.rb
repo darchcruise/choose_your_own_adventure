b5df6f02854896254cc221ceb3fed3202f36508a
darchcruise <darchcruise@yahoo.com>
2013-08-06 14:47:34 -0400

    controller update

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -3,20 +3,15 @@ class AdventuresController < ApplicationController
   end
 
   def new
-    @adventure = Adventure.new
+    @adventure = Adventure.new    #create object to be used in form. Info from form is stored in this adventure object.
   end
 
   def create
-    @adventure = Adventure.new(params[:adventure])
-    if @adventure.save
-      redirect_to new_adventure_path, notice: 'Adventure was successfully created.'
-    else
-      render action: "new"
-    end
+    @adventure = Adventure.new(params[:adventure])   # adventure object returns the info from form
   end
 
   def edit
-    @adventure = Adventure.find(params[:id])
+    @adventure = Adventure.find(params[:id])    #info in object gets overwritten by new info entered into the form. Info from form edits previous info stored in this adventure object
   end
 
   def update
